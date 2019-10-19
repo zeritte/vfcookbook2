2a14b6826029b167fba4e29b45a42aeca15b5442
Mert Alpaslan <alpaslanmert@yahoo.com>
2019-10-19 21:40:34 +0300

    Final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   resources :solutions
   resources :vf_cases
   root to: "vf_cases#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_174736) do
+ActiveRecord::Schema.define(version: 2019_10_19_182102) do
 
   create_table "domains", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_10_19_174736) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
